c726e17cf68934e5b71048ec2a4dc19e95f422b8
mondayy1 <hojoong310@gmail.com>
2025-11-21 16:31:50 +0900

    feat: define errors

--- apps/backend/internal/domain/errors.go
+++ apps/backend/internal/domain/errors.go
@@ -1,1 +1,10 @@
 package domain
+
+import "errors"
+
+var (
+	ErrNotFound      = errors.New("not found")
+	ErrAlreadyExists = errors.New("already exsists")
+	ErrInvalidInput  = errors.New("invalid input")
+	ErrInternal      = errors.New("internal error")
+)
