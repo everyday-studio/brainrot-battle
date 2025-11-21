4024d744909e93a9ef8c0c953a1f9f168a108d51
mondayy1 <hojoong310@gmail.com>
2025-11-21 16:23:15 +0900

    feat: user domain

--- apps/backend/internal/domain/user.go
+++ apps/backend/internal/domain/user.go
@@ -1,1 +1,19 @@
 package domain
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type UserRepository interface {
+	Save(user *User) (*User, error)
+	GetByID(id int) (*User, error)
+	GetAll() ([]User, error)
+}
+
+type UserUseCase interface {
+	CreateUser(user *User) (*User, error)
+	GetByID(id int) (*User, error)
+	GetAll() ([]User, error)
+}
